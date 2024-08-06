d92d9001caf98bd492f6818ba087a94629095621
Lauragranados2021 <93840032+Lauragranados2021@users.noreply.github.com>
2024-08-06 16:47:37 -0500

    quitar conexion a mongo

--- drivers/connect-db.js
+++ drivers/connect-db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-const URL="mongodb+srv://jairouptc:nyxmeR-zusjoz-8zubda@cluster0.9lfyrk5.mongodb.net/?retryWrites=true&w=majority"
+//const URL="mongodb+srv://jairouptc:nyxmeR-zusjoz-8zubda@cluster0.9lfyrk5.mongodb.net/?retryWrites=true&w=majority"
 mongoose.set('strictQuery')
 mongoose.connect(URL)
     .then(()=>console.log('Connect Succes'))
